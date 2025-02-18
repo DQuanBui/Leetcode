9ca822eebd449b050d99d688e11ac1a83f183424
Quan Bui <149330855+DQuanBui@users.noreply.github.com>
2025-02-18 01:01:02 -0500

    Time: 698 ms (34.74%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,9 @@
 SELECT 
-    a.name 
+    name 
 FROM 
-    Employee a 
-    JOIN Employee b ON a.id = b.managerId 
-GROUP BY 
-    b.managerId 
-HAVING 
-    COUNT(*) >= 5
+    Employee 
+WHERE id IN (
+    SELECT managerId 
+    FROM Employee 
+    GROUP BY managerId 
+    HAVING COUNT(*) >= 5)

26643827ade7be9646142304d0bf7aa06de914ff
Quan Bui <149330855+DQuanBui@users.noreply.github.com>
2026-03-16 22:00:08 -0400

    Time: 477 ms (42.23%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,7 +1,10 @@
+# id: primary key
+# names > id != 2 or not referred by any customer
+
 SELECT 
-    name 
+    name
 FROM 
-    Customer 
+    Customer
 WHERE 
     referee_id != 2
-    OR referee_id IS null
+    OR referee_id IS NULL

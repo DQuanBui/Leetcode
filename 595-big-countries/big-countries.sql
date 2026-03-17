1d88a831410913e168aa8a0ae904690ee81d01de
Quan Bui <149330855+DQuanBui@users.noreply.github.com>
2026-03-16 22:08:17 -0400

    Time: 270 ms (85.98%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,9 +1,13 @@
+# name: primary key
+# big if: 1. area >= 3000000, or 2. population >= 25000000
+# name, population, and area
+
 SELECT 
     name,
     population,
-    area 
+    area
 FROM 
-    World 
+    World
 WHERE 
     area >= 3000000
     OR population >= 25000000

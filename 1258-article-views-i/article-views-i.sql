0d6207ea669ea7a61121afbaefc2a9d9b509c3fb
Quan Bui <149330855+DQuanBui@users.noreply.github.com>
2026-03-16 22:38:58 -0400

    Time: 439 ms (26.60%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,8 +1,12 @@
+# no primary key, may have duplicate rows
+# author_id and viewer_id: same person
+# find authors that viewed >= their own articles
+
 SELECT 
     DISTINCT(author_id) AS id
 FROM 
-    Views 
+    Views
 WHERE 
-    viewer_id = author_id 
+    author_id = viewer_id
 ORDER BY 
-    author_id 
+    id 

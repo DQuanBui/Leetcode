db27d09ebd6a6a361bd4ce5678ee4cc6e78cfbc7
Quan Bui <149330855+DQuanBui@users.noreply.github.com>
2026-03-17 23:36:15 -0400

    Time: 1136 ms (58.48%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,7 +1,11 @@
-SELECT
-    Eu.unique_id,
-    E.name AS name
+# Employees table > id: primary key
+# EmployeeUNI table > id , unique_id: the primary key
+# find the Unique ID (each user) > replace user (no unique ID) with NULL
+
+SELECT  
+    eu.unique_id,
+    e.name
 FROM 
-    Employees E
-    LEFT JOIN EmployeeUNI Eu
-    ON E.id = Eu.id 
+    Employees e
+    LEFT JOIN EmployeeUNI eu
+    ON e.id = eu.id

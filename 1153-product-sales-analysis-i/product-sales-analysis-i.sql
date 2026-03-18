5b37b9eb023122b1bb9785e9db862c19d9499586
Quan Bui <149330855+DQuanBui@users.noreply.github.com>
2026-03-17 23:40:27 -0400

    Time: 1229 ms (51.34%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,8 +1,11 @@
+# sales_id, year: primary key (Sales table); product_id: foreign key (Product table)
+# find product_name, year, price (each sale_id)
+
 SELECT 
     p.product_name,
     s.year,
     s.price
 FROM 
     Sales s
-    JOIN Product p 
-    ON s.product_id = p.product_id 
+    JOIN Product p
+    ON s.product_id = p.product_id
